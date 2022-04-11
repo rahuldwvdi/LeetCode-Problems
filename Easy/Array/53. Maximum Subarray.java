d92a4a7cae27e345caa57357c4ce38b51fbb370e
Rahul Dwivedi <51382525+rahuldwvdi@users.noreply.github.com>
2022-04-11 16:54:21 +0530

    sliding window

---
+++ Easy/Array/53. Maximum Subarray.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int currsum = 0;
+        int maxsum = Integer.MIN_VALUE;
+        for(int i:nums){
+            currsum += i;
+            maxsum = Math.max(currsum, maxsum);
+            if(currsum<0){  //if currsum goes below zero then it cannot contribute to maxsum
+                currsum = 0;
+            }
+        }
+        return maxsum;
+    }
+}
