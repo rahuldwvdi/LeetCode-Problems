a5b1f70d6259f2d9959feeb557bed9613033d766
Rahul Dwivedi <51382525+rahuldwvdi@users.noreply.github.com>
2023-06-28 04:51:17 +0530

    added presum approach

--- Medium/Array/974. Subarray Sums Divisible by K.java
+++ Medium/Array/974. Subarray Sums Divisible by K.java
@@ -1,8 +1,9 @@
 class Solution {
     public int subarraysDivByK(int[] nums, int k) {
-        /* //brute force -> solves but time complexity is very high
         int n = nums.length;
-        int count =0;
+        int count = 0;
+        /* 
+        //brute force -> 38/73 test case passed, TC O(n3)
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
                 int sum = 0;
@@ -11,6 +12,16 @@ class Solution {
             }
         }
         return count;
+        //prefix sum approach -> 69/73 test case passed, TC O(n2)
+        for(int i=0;i<n;i++){
+            int presum = 0;
+            for(int j=i;j<n;j++){
+                presum += nums[j];
+                if(presum%k==0) count++;
+            }
+        }
+        return count;
+        */
         */ 
     }
 }
