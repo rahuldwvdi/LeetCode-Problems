a65b6ca178b5e1d0c5fe42c8ca59038918e970ee
Rahul Dwivedi <51382525+rahuldwvdi@users.noreply.github.com>
2023-06-28 04:17:47 +0530

    Create 974. Subarray Sums Divisible by K.java
    brute force

---
+++ Medium/Array/974. Subarray Sums Divisible by K.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+        /* //brute force -> solves but time complexity is very high
+        int n = nums.length;
+        int count =0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                int sum = 0;
+                for(int a=i;a<=j;a++)   sum += nums[a];
+                if(sum%k==0) count++;
+            }
+        }
+        return count;
+        */
+      
+    }
+}
